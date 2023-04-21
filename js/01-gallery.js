866dbf4884e6cf8ac074f09977c66e747a349113
Illia Shatokhin <resonance.2106@gmail.com>
2023-04-21 21:53:10 +0300

    first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const listItemString = galleryItems
+    .map(({preview, original, description}) => `<li class="gallery__item"><a class="gallery__link" href="${original}"><img
+class="gallery__image"
+src="${preview}"
+data-source="${original}"
+alt="${description}"
+/></a></li>`)
+    .join("");
+
+galleryList.insertAdjacentHTML("afterbegin", listItemString);
+
+galleryList.addEventListener("click", openOriginalSize);
+
+function openOriginalSize (event) {
+    event.preventDefault();
+    if (event.target.nodeName === "IMG") {
+        const original = event.target.getAttribute("data-source");
+        const instance = basicLightbox.create(`<img src="${original}">`);
+        instance.show();
+        document.addEventListener("keydown", function closeModalOnEsc (event) {
+            if (event.code === "Escape") {
+                instance.close();
+                document.removeEventListener("keydown", closeModalOnEsc)
+            };
+        });
+    };
+};
