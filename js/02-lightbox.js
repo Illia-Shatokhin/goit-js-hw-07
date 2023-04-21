9e23c79861d1bebd23f2719f3eefa0bba5ec526a
Illia Shatokhin <resonance.2106@gmail.com>
2023-04-21 22:25:11 +0300

    first finish

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const listItemString = galleryItems
+    .map(({preview, original, description}) => `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`)
+    .join("");
+
+galleryList.insertAdjacentHTML("afterbegin", listItemString);
+
+galleryList.addEventListener("click", openOriginalSize);
+
+function openOriginalSize(event) {
+    event.preventDefault();
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "outside",
+});
