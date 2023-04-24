3163ba068ba0ae178fcc7deaee8fdca9aba058ff
Illia Shatokhin <resonance.2106@gmail.com>
2023-04-24 21:52:28 +0300

    prettier format

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,31 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
 const galleryList = document.querySelector(".gallery");
 
 const listItemString = galleryItems
-    .map(({preview, original, description}) => `<li class="gallery__item"><a class="gallery__link" href="${original}"><img
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<li class="gallery__item"><a class="gallery__link" href="${original}"><img
 class="gallery__image"
 src="${preview}"
 data-source="${original}"
 alt="${description}"
-/></a></li>`)
-    .join("");
+/></a></li>`
+  )
+  .join("");
 
 galleryList.insertAdjacentHTML("afterbegin", listItemString);
 
 galleryList.addEventListener("click", openOriginalSize);
 
-function openOriginalSize (event) {
-    event.preventDefault();
-    if (event.target.nodeName === "IMG") {
-        const original = event.target.getAttribute("data-source");
-        const instance = basicLightbox.create(`<img src="${original}">`);
-        instance.show();
-        document.addEventListener("keydown", function closeModalOnEsc (event) {
-            if (event.code === "Escape") {
-                instance.close();
-                document.removeEventListener("keydown", closeModalOnEsc)
-            };
-        });
-    };
-};
+function openOriginalSize(event) {
+  event.preventDefault();
+  if (event.target.nodeName === "IMG") {
+    const original = event.target.getAttribute("data-source");
+    const instance = basicLightbox.create(`<img src="${original}">`);
+    instance.show();
+    document.addEventListener("keydown", function closeModalOnEsc(event) {
+      if (event.code === "Escape") {
+        instance.close();
+        document.removeEventListener("keydown", closeModalOnEsc);
+      }
+    });
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,15 +1,18 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
 const galleryList = document.querySelector(".gallery");
 
 const listItemString = galleryItems
-    .map(({preview, original, description}) => `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`)
-    .join("");
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`
+  )
+  .join("");
 
 galleryList.insertAdjacentHTML("afterbegin", listItemString);
 
-const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: "alt",
-    captionDelay: 250,
-    captionPosition: "outside",
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "outside",
 });
